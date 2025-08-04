377837d15af88c2deb0dcb9d01f8be69f563e557
phi1omaly <philosophicalanomaly@outlook.com>
2025-08-04 11:12:13 -0400

    login contoller added

---
+++ src/main/java/com/main/m_cogdell_libraryassignment4/loginViewController.java
@@ -0,0 +1,27 @@
+package com.main.m_cogdell_libraryassignment4;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class loginViewController implements Initializable {
+    @FXML
+    private TextField userNameField;
+    private PasswordField passwordField;
+    private Button loginButton;
+    private MenuItem file;
+    private MenuItem edit;
+    private MenuItem help;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+}
